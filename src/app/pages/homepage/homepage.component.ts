1484c4d1004e88165bba2b89770793c7f5db10dd
G-raph <info@g-raph.be>
2021-10-30 16:22:48 +0200

    homepage teasers

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -3,6 +3,9 @@ import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/api.service';
 import { Article } from 'src/app/articles/article';
 import { Category } from 'src/app/categories/category';
+import { EventItem } from 'src/app/events/event-item';
+import { Sponsor } from 'src/app/sponsors/sponsor';
+import { Team } from 'src/app/teams/team';
 import { environment } from 'src/environments/environment';
 import { Homepage } from './homepage';
 
@@ -15,7 +18,9 @@ export class HomepageComponent implements OnInit {
 
   homepage$: Observable<Homepage>;
   articles$: Observable<Article[]>;
-  categories$: Observable<Category[]>;
+  events$: Observable<EventItem[]>;
+  sponsors$: Observable<Sponsor[]>;
+  teams$: Observable<Team[]>;
   selected: Date | null;
 
   constructor(private api: ApiService) { }
@@ -23,11 +28,9 @@ export class HomepageComponent implements OnInit {
   ngOnInit() {
     this.homepage$ = this.api.getHomePage();
     this.articles$ = this.api.getArticles();
-    this.articles$.subscribe(console.log);
-  }
-
-  catImageUrl(path: string) {
-    return environment.apiUrl + path;
+    this.events$ = this.api.getTimeslots();
+    this.sponsors$ = this.api.getSponsors();
+    this.teams$ = this.api.getTeams();
   }
 
 }
