6582d3bb94412830eae116c3bc76547125cd47fa
Gielen Gunter <Gunter.Gielen@cegeka.com>
2021-11-17 13:43:44 +0100

    logo/color change + only future events

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 import { ApiService } from 'src/app/api.service';
 import { environment } from 'src/environments/environment';
 import { EventItem } from '../event-item';
@@ -9,19 +10,28 @@ import { EventItem } from '../event-item';
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.scss']
 })
-export class EventListComponent implements OnInit {
+export class EventListComponent implements OnInit, OnDestroy {
 
   events$: Observable<EventItem[]>;
+  destroy$ = new Subject();
 
   constructor(private api: ApiService) {
   }
 
   ngOnInit(): void {
-    this.events$ = this.api.getTimeslots();
+    this.events$ = this.api.getTimeslots().pipe(
+      takeUntil(this.destroy$),
+      map(items => items.filter(item => new Date().setHours(0,0,0,0) <= new Date(item.startdate).setHours(0,0,0,0)))
+    );
   }
 
   catImageUrl(path: string) {
     return environment.apiUrl + path;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
