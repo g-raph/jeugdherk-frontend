412988d495c533d10ee1a0eea0bfc2c660eaa706
Gunter Gielen <gunter.gielen@telenet.be>
2023-02-09 09:26:09 +0100

    event meta

--- src/app/events/event-detail/event-detail.component.ts
+++ src/app/events/event-detail/event-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { ApiService } from 'src/app/api.service';
 import { environment } from 'src/environments/environment';
 import { EventItem } from '../event-item';
@@ -10,14 +11,74 @@ import { EventItem } from '../event-item';
   templateUrl: './event-detail.component.html',
   styleUrls: ['./event-detail.component.scss']
 })
-export class EventDetailComponent implements OnInit {
+export class EventDetailComponent implements OnInit, OnDestroy {
 
   page$: Observable<EventItem>;
 
-  constructor(private route: ActivatedRoute, private api: ApiService) { }
+  metaSubscription: Subscription;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private api: ApiService,
+    private title: Title,
+    private meta: Meta
+  ) { }
 
   ngOnInit() {
     this.page$ = this.api.getTimeslot(this.route.snapshot.params.slug);
+
+    this.metaSubscription = this.page$.subscribe(data => {
+      this.title.setTitle('Jeugdvoetbal Herk | ' + data.title);
+      const keywords = data.title.split(' ').join(',');
+      const description = data.text;
+
+      this.meta.updateTag({
+          'name': 'keywords',
+          'content': keywords
+        },
+        'name="keywords"'
+      );
+      this.meta.updateTag({
+          'name': 'description',
+          'content': description
+        }, 
+        'name="description"'
+      );
+      this.meta.updateTag({
+          'property': 'og:title',
+          'content': 'JVGH | ' + data.title
+        }, 
+        'property="og:title"'
+      );
+      this.meta.updateTag({
+          'property': 'og:description',
+          'content': data.text.slice(0, 50) + '...'
+        }, 
+        'property="og:description"'
+      );
+      this.meta.updateTag({
+          'property': 'og:image',
+          'content': data.images[0].url
+        }, 
+        'property="og:image"'
+      );
+      this.meta.updateTag({
+          'property': 'og:type',
+          'content': 'website'
+        }, 
+        'property="og:type"'
+      );
+      this.meta.updateTag({
+          'property': 'og:url',
+          'content': window.location.href
+        }, 
+        'property="og:url"'
+      );
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.metaSubscription.unsubscribe();
   }
 
 }
